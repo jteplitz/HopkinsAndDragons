6f4eb1cf6ccf329ebff3ac86a6def2a68eff4b4b
Jason Teplitz <jteplitz602@gmail.com>
2013-04-14 00:19:59 -0400

    added edit enemies page

--- routes/EditGameEnemies.js
+++ routes/EditGameEnemies.js
@@ -10,26 +10,27 @@
 
           ControllerClass = require("../controllers/EditGameEnemies.js");
 
-    handleGet = function(req, res, next){
-    
-    var control = new ControllerClass();
+  handleGet = function(req, res, next){
+    var control = new ControllerClass(req.session.user, req.params.id, req._schemas);
 
     var params = {};
-
     control.renderView(res, params);
+  };
+
+  handlePost = function(req, res, next){
     
   };
-    handlePost = function(req, res, next){
+
+  handlePut = function(req, res, next){
     
   };
-    handlePut = function(req, res, next){
-    
-  };
-    handleDelete = function(req, res, next){
+
+  handleDelete = function(req, res, next){
     
   };
   
-  dispatch = {    GET: handleGet,        POST: handlePost,        PUT: handlePut,        DELETE: handleDelete,    };  handler = function(req, res, next){
+  dispatch = {GET: handleGet, POST: handlePost, PUT: handlePut, DELETE: handleDelete};
+  handler = function(req, res, next){
     if (_.has(dispatch, req.method)){
       return dispatch[req.method](req, res, next);
     }
