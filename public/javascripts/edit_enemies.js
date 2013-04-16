f1fe122b7bdf7f72cabc88e273eed5d87ef8dbda
Jason Teplitz <Jason@Jason-Teplitzs-MacBook-Pro.local>
2013-04-15 23:40:44 -0400

    enemies can be placcwed on the map and their pull radius altered.
    Still need to allow for saving of enemies, better enemy selection, and enemy deletion

--- public/javascripts/edit_enemies.js
+++ public/javascripts/edit_enemies.js
@@ -1,26 +1,132 @@
-/*globals dragons*/
+/*globals dragons _*/
 (function(){
   "use strict";
 
   // globals
-  var canvas, mapPieces = [],
+  var canvas, mapPieces = [], dragging = false, selectedEnemy,
 
   // functions
-  main, restoreMap, addPiece, RoomElement;
+  main, restoreMap, addPiece, RoomElement, PullRadius, Enemy,
+  startDragging, dragPiece, stopDragging, removeDraggingPullRadius, changeRadius;
 
   dragons.organizedMap = {};
 
   $(document).ready(function(){
+    $(".baseEnemy").on("mousedown", startDragging);
+    $(document).on("mousemove", dragPiece);
+    $(document).on("mouseup", ".dragging", stopDragging);
+    $(".baseEnemy").on("dragstart", function(e){ e.preventDefault() }); // prevent browser dragging from getting in the way
+
+    $(".enemyProperties .pullRadius").on("change", changeRadius);
+
     canvas = new dragons.canvas($("#map")[0]);
-    
     restoreMap();
-    setInterval(main, 50); // main canvas loop
+    setInterval(main, 20); // main canvas loop
   });
 
   main = function(){
     canvas.update();
   };
 
+  startDragging = function(e){
+    if (!dragging){
+      var baseEnemy = null;
+      // populate the stats
+      for (var i = 0; i < dragons.baseEnemies.length; i++){
+        if (dragons.baseEnemies[i]._id === $(this).attr("data-id")){
+          baseEnemy = dragons.baseEnemies[i];
+          break;
+        }
+      }
+      $("h3.enemyName").text(baseEnemy.name);
+      $(".enemyProperties .level").val(baseEnemy.level);
+      $(".enemyProperties .type").val(baseEnemy.type);
+      $(".enemyProperties .health").val(baseEnemy.health);
+      $(".enemyProperties .armor").val(baseEnemy.armor);
+      $(".enemyProperties .pullRadius").val(20);
+
+      $(".enemyProperties").removeClass("hidden");
+
+      dragging = true;
+
+      var piece = $(this).clone();
+      piece.addClass("dragging");
+      piece.attr("data-startX", $(this).offset().left);
+      piece.attr("data-startY", $(this).offset().top);
+      $("body").append(piece);
+
+      dragPiece(e);
+    }
+  };
+
+  dragPiece = function(e){
+    var i;
+    if (dragging){
+      $(".dragging").css("top", e.pageY + "px");
+      $(".dragging").css("left", e.pageX + "px");
+
+      if (dragons.utils.detectMouseOver($("#map"), e)){
+        // if the piece is over the map display its pull radius (20 px by default)
+        var x = ($(".dragging").position().left - $("#map").offset().left) + ($(".dragging").width() / 2);
+        var y = ($(".dragging").position().top - $("#map").offset().top)  + ($(".dragging").height() / 2);
+
+        // remove the old dragging pull radius
+        removeDraggingPullRadius();
+
+        var pullRadius = new PullRadius(20, x, y, "#32B7F0");
+        pullRadius.dragging = true;
+        canvas.elements.push(pullRadius);
+      } else {
+        // remove the old dragging pull radius if it was there
+        removeDraggingPullRadius();
+      }
+    }
+  };
+
+  stopDragging = function(e){
+    dragging = false;
+    // TODO Make sure enemies can't collide, and check wall collision, and check that we're on a piece
+    if (dragons.utils.detectMouseOver($("#map"), e)){
+      var enemyImage = new Image();
+
+      enemyImage.onload = function(){
+        var enemy = new Enemy(enemyImage, $(".dragging").width(), $(".dragging").height(),
+                              e.pageX - $("#map").offset().left, e.pageY - $("#map").offset().top, 20, $(".dragging").attr("data-id"));
+        canvas.elements.push(enemy);
+        $(".dragging").remove();
+        removeDraggingPullRadius();
+        selectedEnemy = enemy;
+      };
+
+      enemyImage.src = $(".dragging").attr("src");
+    } else { // fly the enemy away
+      $(".dragging").animate({
+        left: $(".dragging").attr("data-startX") + "px",
+        top: $(".dragging").attr("data-startY") + "px"
+      }, {
+        duration: 200,
+        complete: function(){
+          $(this).remove();
+        }
+      });
+    }
+  };
+
+  changeRadius = function(){
+    var radius = $(this).val();
+
+    selectedEnemy.setRadius(radius);
+  };
+
+  removeDraggingPullRadius = function(){
+    var i;
+    for (i = 0; i < canvas.elements.length; i++){
+      if (canvas.elements[i] instanceof PullRadius && canvas.elements[i].dragging){
+        canvas.elements.splice(i, 1);
+        break;
+      }
+    }
+  };
 
   // maybe place the following three functions in some other file? canvas.js? map.js...
   restoreMap = function(){
@@ -49,5 +155,40 @@
     this.doorRight  = doorRight;
     this.doorTop    = doorTop;
     this.doorBottom = doorBottom;
+  };
+
+  PullRadius = function(radius, x, y, color){
+    dragons.gameElements.element.call(this, radius, radius, x, y, null);
+
+    this.radius = radius;
+    this.color  = color;
+
+    this.draw = function(ctx){
+      ctx.save();
+      
+      ctx.globalAlpha = 0.7;
+      ctx.beginPath();
+      ctx.fillStyle = this.color;
+      ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
+      ctx.fill();
+
+      ctx.restore();
+    };
+  };
+
+  Enemy = function(image, width, height, x, y, pullRadius, id){
+    dragons.gameElements.element.call(this, width, height, x, y, id);
+
+    this.image      = new dragons.gameElements.image(image, width, height, x, y, 0, id);
+    this.pullRadius = new PullRadius(pullRadius, x + (width / 2), y + (height / 2), "#32B7F0");
+
+    this.draw = function(ctx){
+      this.pullRadius.draw(ctx);
+      this.image.draw(ctx);
+    };
+
+    this.setRadius = function(radius){
+      this.pullRadius = new PullRadius(radius, x + (width / 2), y + (height / 2), "#32B7F0");
+    };
   };
 }());
