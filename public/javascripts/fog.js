049f81bd6293795aa6a12920091f2a0d32c985cc
Jason Teplitz <jteplitz602@gmail.com>
2013-05-06 20:59:36 -0400

    First shot at fog of war.
    The fog works locally, but doesn't yet save on the server or update on other clients

---
+++ public/javascripts/fog.js
@@ -0,0 +1,26 @@
+/*globals dragons*/
+(function(){
+  "use strict";
+
+  dragons.fogCanvas = function(canvas, width, height){
+    this.ctx    = canvas.getContext("2d");
+    this.width  = width;
+    this.height = height;
+    this.sight  = dragons.globals.playerSight;
+
+    this.ctx.fillStyle = "rgba(0, 0, 0, 1)";
+    this.ctx.fillRect(0, 0, width, height);
+
+    this.updateFog = function(player){
+      this.ctx.fillStyle = "rgba(200, 200, 200, 1)";
+      for (var i = 0; i < player.movements.length; i++){
+        var movement  = player.movements[i];
+        this.ctx.beginPath();
+        this.ctx.globalCompositeOperation = "destination-out";
+        this.ctx.arc(movement.x, movement.y, this.sight, 0, 2 * Math.PI, false);
+        this.ctx.fill();
+      }
+      player.movements.splice(0);
+    };
+  };
+}());
